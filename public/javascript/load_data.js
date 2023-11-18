233024fbdf23d840e363dc09fe5d4c35522ae4d2
JorgeMawyin <jmawyin@espol.edu.ec>
2023-11-17 19:37:39 -0500

    Dashboard

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -71,10 +71,19 @@ let cargarOpenMeteo = () => {
         datasets: [
           {
             label: 'Temperature [2m]',
-            data: data, 
+            data: data,
+            backgroundColor: [
+              '#00A9FF'
+            ],
+            borderColor: [
+              '#89CFF3'
+            ],
+            width: 50
           }
         ]
+        
       }
+      
     };
 
     //Objeto con la instanciación del gráfico
@@ -116,7 +125,13 @@ let cargarOpenMeteo2 = () => {
         datasets: [
           {
             label: 'Relative Humidity [2m]',
-            data: data, 
+            data: data,
+            backgroundColor: [
+              '#00A9FF'
+            ],
+            borderColor: [
+              '#89CFF3'
+            ]
           }
         ]
       }
@@ -130,3 +145,118 @@ let cargarOpenMeteo2 = () => {
 
 }
 cargarOpenMeteo2()
+
+let parseXML = (responseText) => {
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(responseText, "application/xml");
+
+  // Referencia al elemento `#forecastbody` del documento HTML
+
+  let forecastElement = document.querySelector("#forecastbody");
+  forecastElement.innerHTML = "";
+
+  // Procesamiento de los elementos con etiqueta `<time>` del objeto xml
+  let timeArr = xml.querySelectorAll("time");
+
+  timeArr.forEach((time) => {
+    let from = time.getAttribute("from").replace("T", " ");
+
+    let humidity = time.querySelector("humidity").getAttribute("value");
+    let windSpeed = time.querySelector("windSpeed").getAttribute("mps");
+    let precipitation = time
+      .querySelector("precipitation")
+      .getAttribute("probability");
+    let pressure = time.querySelector("pressure").getAttribute("value");
+    let cloud = time.querySelector("clouds").getAttribute("all");
+
+    let template = `
+          <tr>
+              <td>${from}</td>
+              <td>${humidity}</td>
+              <td>${windSpeed}</td>
+              <td>${precipitation}</td>
+              <td>${pressure}</td>
+              <td>${cloud}</td>
+          </tr>
+      `;
+
+    //Renderizando la plantilla en el elemento HTML
+    forecastElement.innerHTML += template;
+  });
+};
+
+// Callback async
+let selectListener = async (event) => {
+  let selectedCity = event.target.value;
+
+  let cityStorage = localStorage.getItem(selectedCity);
+
+  if (cityStorage == null) {
+    try {
+      //API key
+      let APIkey = "d33cc0a95aca9c04cf35531de58b502d";
+      let url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&mode=xml&appid=${APIkey}`;
+
+      let response = await fetch(url);
+      let responseText = await response.text();
+
+      await localStorage.setItem(selectedCity, responseText);
+
+      await parseXML(responseText);
+
+      // Guarde la entrada de almacenamiento local
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    // Procese un valor previo
+    parseXML(cityStorage);
+  }
+};
+
+let loadForecastByCity = () => {
+  //Handling event
+  let selectElement = document.querySelector("select");
+  selectElement.addEventListener("change", selectListener);
+};
+
+let verifyLocalTime = () => {};
+
+let loadExternalTable = async () => {
+  //Requerimiento asíncrono
+  let url = "https://www.gestionderiesgos.gob.ec/monitoreo-de-inundaciones/";
+
+  //cors proxy
+  let proxyURL = 'https://cors-anywhere.herokuapp.com/'
+  let endpoint = proxyURL + url
+
+  let response = await fetch(endpoint);
+  let responseText = await response.text();
+
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(responseText, "text/html");
+
+  console.log("que mirah bobo" + xml);
+
+  let elementoXML = xml.querySelector("#postcontent table");
+  let elementoDOM = document.getElementById("monitoreo");
+
+  elementoDOM.innerHTML = elementoXML.outerHTML;
+};
+
+loadExternalTable();
+loadForecastByCity();
+
+//MODO OSCURO - CLARO
+let toggle = document.getElementById('toggleDark');
+toggle.addEventListener('change', (event)=>{
+  let checked = event.target.checked;
+  document.body.classList.toggle('dark');
+  document.querySelector('.headerLight').classList.toggle('headerDark');
+  document.querySelector('.card-body-precipitacion-L').classList.toggle('card-body-precipitacion-D');
+  document.querySelector('.card-body-uv-L').classList.toggle('card-body-uv-D');
+  document.querySelector('.card-body-temperatura-L').classList.toggle('card-body-temperatura-D');
+  document.querySelector('.card-body-map-L').classList.toggle('card-body-map-D');
+  document.querySelector('.card-body-pronostico-L').classList.toggle('card-body-pronostico-D');
+  document.querySelector('.form-select-L').classList.toggle('form-select-D');
+});
